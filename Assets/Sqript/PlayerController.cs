5dfc85672c19de88e64cfbd9d60192becaba1fb9
H1mar1 <1226himari@au.com>
2025-05-08 11:59:36 +0900

    ジャンプの実装

--- Assets/Sqript/PlayerController.cs
+++ Assets/Sqript/PlayerController.cs
@@ -6,6 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed = 3.0f;
+
+    private Rigidbody2D rbody2D;
+    private float jumpFprce = 200f;//ジャンプの高さ
+    private int jumpCount = 0;//ジャンプの回数
+
+    private void Start()
+    {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         // 右に移動
@@ -18,6 +27,20 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.position -= speed * transform.right * Time.deltaTime;
+        }
+        //ジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) && this.jumpCount < 2)//２段ジャンプあり
+        {
+            this.rbody2D.AddForce(transform.up * jumpFprce);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 0;//地面に付いたらジャンプのカウントを0にする
         }
     }
 }
